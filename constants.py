cd58acd5d275789f389a519799df5b70b4a16bd6
soni <acdsoni@gmail.com>
2024-02-03 19:01:00 +0100

    created constants file

---
+++ constants.py
@@ -0,0 +1,3 @@
+DATABASE_MAIN = "main"
+DATABASE_TEST = "test"
+MONGO_CONNECTION_STRING="mongodb+srv://typetone:Hjw9iRm74cY4qpBS@cluster0.7dxtavv.mongodb.net/"
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pymongo import MongoClient
 from fastapi import FastAPI
 from routes import router
+from constants import DATABASE_MAIN, MONGO_CONNECTION_STRING
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
     # The connection string here is dangerously exposed in the code for simplicity sake, it should never be done this way in a real prod env
-    app.mongodb_client = MongoClient("mongodb+srv://typetone:Hjw9iRm74cY4qpBS@cluster0.7dxtavv.mongodb.net/")
-    app.database = app.mongodb_client["test"]
+    app.mongodb_client = MongoClient(MONGO_CONNECTION_STRING)
+    app.database = app.mongodb_client[DATABASE_MAIN]
 
 @app.on_event("shutdown")
 def shutdown_db_client():
